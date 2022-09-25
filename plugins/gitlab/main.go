0217eb5d09548c0ab6c4960dba8c69de244e314e
Evgenii Protsenko <procenkoegg@gmail.com>
2022-09-25 10:52:42 +0000

    'cli + plugin' skeleton

---
+++ plugins/gitlab/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"os"
+
+	"github.com/hashicorp/go-hclog"
+	"github.com/hashicorp/go-plugin"
+	"github.com/scan-io-git/scan-io/shared"
+)
+
+// Here is a real implementation of VCS
+type VCSHello struct {
+	logger hclog.Logger
+}
+
+func (g *VCSHello) Fetch() string {
+	g.logger.Debug("message from VCSHello.Fetch")
+	return "Hello!"
+}
+
+// handshakeConfigs are used to just do a basic handshake between
+// a plugin and host. If the handshake fails, a user friendly error is shown.
+// This prevents users from executing bad plugins or executing a plugin
+// directory. It is a UX feature, not a security feature.
+var handshakeConfig = plugin.HandshakeConfig{
+	ProtocolVersion:  1,
+	MagicCookieKey:   "BASIC_PLUGIN",
+	MagicCookieValue: "hello",
+}
+
+func main() {
+	logger := hclog.New(&hclog.LoggerOptions{
+		Level:      hclog.Trace,
+		Output:     os.Stderr,
+		JSONFormat: true,
+	})
+
+	VCS := &VCSHello{
+		logger: logger,
+	}
+	// pluginMap is the map of plugins we can dispense.
+	var pluginMap = map[string]plugin.Plugin{
+		"vcs": &shared.VCSPlugin{Impl: VCS},
+	}
+
+	logger.Debug("message from plugin", "foo", "bar")
+
+	plugin.Serve(&plugin.ServeConfig{
+		HandshakeConfig: handshakeConfig,
+		Plugins:         pluginMap,
+	})
+}
---
+++ shared/ivcs.go
@@ -0,0 +1,62 @@
+package shared
+
+import (
+	"net/rpc"
+
+	"github.com/hashicorp/go-plugin"
+)
+
+// VCS is the interface that we're exposing as a plugin.
+type VCS interface {
+	Fetch() string
+}
+
+// Here is an implementation that talks over RPC
+type VCSRPC struct{ client *rpc.Client }
+
+func (g *VCSRPC) Fetch() string {
+	var resp string
+	err := g.client.Call("Plugin.Fetch", new(interface{}), &resp)
+	if err != nil {
+		// You usually want your interfaces to return errors. If they don't,
+		// there isn't much other choice here.
+		panic(err)
+	}
+
+	return resp
+}
+
+// Here is the RPC server that VCSRPC talks to, conforming to
+// the requirements of net/rpc
+type VCSRPCServer struct {
+	// This is the real implementation
+	Impl VCS
+}
+
+func (s *VCSRPCServer) Fetch(args interface{}, resp *string) error {
+	*resp = s.Impl.Fetch()
+	return nil
+}
+
+// This is the implementation of plugin.Plugin so we can serve/consume this
+//
+// This has two methods: Server must return an RPC server for this plugin
+// type. We construct a VCSRPCServer for this.
+//
+// Client must return an implementation of our interface that communicates
+// over an RPC client. We return VCSRPC for this.
+//
+// Ignore MuxBroker. That is used to create more multiplexed streams on our
+// plugin connection and is a more advanced use case.
+type VCSPlugin struct {
+	// Impl Injection
+	Impl VCS
+}
+
+func (p *VCSPlugin) Server(*plugin.MuxBroker) (interface{}, error) {
+	return &VCSRPCServer{Impl: p.Impl}, nil
+}
+
+func (VCSPlugin) Client(b *plugin.MuxBroker, c *rpc.Client) (interface{}, error) {
+	return &VCSRPC{client: c}, nil
+}
