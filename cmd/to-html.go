871e132cfe64998d55a7f40c677f0c54a3ee4b26
Evgenii Protsenko <procenkoegg@gmail.com>
2024-06-15 13:03:43 +0200

    Move all sarif processing to a separete internal package

--- cmd/to-html.go
+++ cmd/to-html.go
@@ -1,13 +1,8 @@
 package cmd
 
 import (
-	"bufio"
-	"crypto/md5"
-	"encoding/hex"
-	"encoding/json"
 	"fmt"
 	"html/template"
-	"io"
 	"os"
 	"path/filepath"
 	"sort"
@@ -18,7 +13,7 @@ import (
 	"github.com/owenrumney/go-sarif/v2/sarif"
 	"github.com/spf13/cobra"
 
-	"github.com/scan-io-git/scan-io/pkg/shared/files"
+	scaniosarif "github.com/scan-io-git/scan-io/internal/sarif"
 	"github.com/scan-io-git/scan-io/pkg/shared/logger"
 )
 
@@ -31,184 +26,6 @@ type ToHTMLOptions struct {
 }
 
 var allToHTMLOptions ToHTMLOptions
-
-// enrichResultsTitleProperty function enriches sarif results properties with title and description values
-func enrichResultsTitleProperty(sarifReport *sarif.Report) {
-	rulesMap := map[string]*sarif.ReportingDescriptor{}
-	for _, rule := range sarifReport.Runs[0].Tool.Driver.Rules {
-		rulesMap[rule.ID] = rule
-	}
-
-	for _, result := range sarifReport.Runs[0].Results {
-		if rule, ok := rulesMap[*result.RuleID]; ok {
-			if result.Properties == nil {
-				result.Properties = make(map[string]interface{})
-			}
-			if rule.ShortDescription != nil {
-				result.Properties["Title"] = rule.ShortDescription.Text
-			}
-			if rule.FullDescription != nil && rule.FullDescription.Text != nil {
-				result.Properties["Description"] = *rule.FullDescription.Text
-			} else if result.Message.Text != nil {
-				result.Properties["Description"] = *result.Message.Text
-			}
-		}
-	}
-}
-
-// readLineFromFile function reads a line from a file by the given location
-func readLineFromFile(loc *sarif.PhysicalLocation) (string, error) {
-	//return error if allToHTMLOptions.SourceFolder is not specified
-	if allToHTMLOptions.SourceFolder == "" {
-		return "", fmt.Errorf("source folder is not set")
-	}
-
-	// Construct the file path
-	filePath, err := files.ExpandPath(filepath.Join(allToHTMLOptions.SourceFolder, *loc.ArtifactLocation.URI))
-	if err != nil {
-		return "", fmt.Errorf("failed to contruct a file path: %w", err)
-	}
-	// Open the file
-	file, err := os.Open(filePath)
-	if err != nil {
-		return "", fmt.Errorf("failed to open file: %w", err)
-	}
-	defer file.Close()
-
-	// Read the file line by line
-	scanner := bufio.NewScanner(file)
-	currentLine := 0
-	for scanner.Scan() {
-		currentLine++
-		if currentLine == *loc.Region.StartLine {
-			return scanner.Text(), nil
-		}
-	}
-
-	if err := scanner.Err(); err != nil {
-		return "", fmt.Errorf("error reading file: %w", err)
-	}
-
-	return "", fmt.Errorf("line %d not found in file", *loc.Region.StartLine)
-}
-
-// enrichResultsLocationProperty function enriches sarif location properties with source code and URI values
-func enrichResultsLocationProperty(location *sarif.Location) error {
-	artifactLocation := location.PhysicalLocation.ArtifactLocation
-	if artifactLocation.Properties == nil {
-		artifactLocation.Properties = make(map[string]interface{})
-	}
-	artifactLocation.Properties["URI"] = *artifactLocation.URI
-
-	if location.PhysicalLocation.Region.Properties == nil {
-		location.PhysicalLocation.Region.Properties = make(map[string]interface{})
-	}
-	if location.PhysicalLocation.Region.StartColumn != nil {
-		location.PhysicalLocation.Region.Properties["StartColumn"] = *location.PhysicalLocation.Region.StartColumn - 1
-	} else {
-		location.PhysicalLocation.Region.Properties["StartColumn"] = 0
-	}
-	if location.PhysicalLocation.Region.EndColumn != nil {
-		location.PhysicalLocation.Region.Properties["EndColumn"] = *location.PhysicalLocation.Region.EndColumn - 1
-	} else {
-		location.PhysicalLocation.Region.Properties["EndColumn"] = 0
-	}
-	if location.PhysicalLocation.Region.StartLine != nil {
-		location.PhysicalLocation.Region.Properties["StartLine"] = *location.PhysicalLocation.Region.StartLine
-	} else {
-		location.PhysicalLocation.Region.Properties["StartLine"] = 0
-	}
-	if location.PhysicalLocation.Region.EndLine != nil {
-		location.PhysicalLocation.Region.Properties["EndLine"] = *location.PhysicalLocation.Region.EndLine
-	} else {
-		location.PhysicalLocation.Region.Properties["EndLine"] = location.PhysicalLocation.Region.Properties["StartLine"]
-	}
-
-	// return if allToHTMLOptions.SourceFolder is not specified
-	if allToHTMLOptions.SourceFolder == "" {
-		return fmt.Errorf("source folder is not set")
-	}
-	codeLine, err := readLineFromFile(location.PhysicalLocation)
-	if err != nil {
-		return err
-	}
-	// print amount of spaces bnefore code
-	// spacePrefixLength := len(codeLine) - len(strings.TrimLeft(codeLine, " "))
-	// artifactLocation.Properties["Code"] = strings.TrimLeft(codeLine, " ")
-	artifactLocation.Properties["Code"] = codeLine
-
-	return nil
-}
-
-// enrichResultsCodeFlowProperty function enriches code flow location properties with source code and URI values
-func enrichResultsCodeFlowProperty(sarifReport *sarif.Report) {
-	// init logger
-	logger := logger.NewLogger(AppConfig, "core")
-
-	for _, result := range sarifReport.Runs[0].Results {
-
-		if len(result.CodeFlows) == 0 && len(result.Locations) > 0 {
-			//add new code flow
-			codeFlow := sarif.NewCodeFlow()
-			for _, location := range result.Locations {
-				threadFlow := sarif.NewThreadFlow()
-				threadFlow.Locations = append(threadFlow.Locations, &sarif.ThreadFlowLocation{
-					Location: location,
-				})
-				codeFlow.ThreadFlows = append(codeFlow.ThreadFlows, threadFlow)
-			}
-			result.CodeFlows = append(result.CodeFlows, codeFlow)
-		}
-
-		for _, codeflow := range result.CodeFlows {
-			for _, threadflow := range codeflow.ThreadFlows {
-				for _, location := range threadflow.Locations {
-					err := enrichResultsLocationProperty(location.Location)
-					if err != nil {
-						logger.Debug("can't read source file", "err", err)
-						continue
-					}
-				}
-			}
-		}
-	}
-}
-
-// function to enrich results properties with level taken from corersponding rules propertiues "problem.severity" field
-func enrichResultsLevelProperty(sarifReport *sarif.Report) {
-	rulesMap := map[string]*sarif.ReportingDescriptor{}
-	for _, rule := range sarifReport.Runs[0].Tool.Driver.Rules {
-		rulesMap[rule.ID] = rule
-	}
-
-	for _, result := range sarifReport.Runs[0].Results {
-		if rule, ok := rulesMap[*result.RuleID]; ok {
-			if result.Properties["Level"] == nil {
-				if result.Level != nil {
-					// used by snyk
-					result.Properties["Level"] = *result.Level
-				} else if rule.Properties["problem.severity"] != nil {
-					// used by codeql
-					result.Properties["Level"] = rule.Properties["problem.severity"]
-				} else if rule.DefaultConfiguration != nil {
-					// used by all tools?
-					result.Properties["Level"] = rule.DefaultConfiguration.Level
-				} else {
-					// just a fallback, should never happen
-					result.Properties["Level"] = "unknown"
-				}
-			}
-		}
-	}
-}
-
-// enrichResultsProperties function enriches sarif results properties with title, description, location and level values
-// for better html report representation
-func enrichResultsProperties(sarifReport *sarif.Report) {
-	enrichResultsTitleProperty(sarifReport)
-	enrichResultsCodeFlowProperty(sarifReport)
-	enrichResultsLevelProperty(sarifReport)
-}
 
 // add adds two integers and returns the result.
 // helper function for html template
@@ -224,22 +41,6 @@ func generateSequence(n int) []int {
 		sequence = append(sequence, i)
 	}
 	return sequence
-}
-
-// readSarifReport function reads a sarif report from a file
-// helper function for html template
-func readSarifReport() (*sarif.Report, error) {
-	jsonFile, err := os.Open(allToHTMLOptions.Input)
-	if err != nil {
-		return nil, err
-	}
-	defer jsonFile.Close()
-
-	var sarifReport sarif.Report
-	byteValue, _ := io.ReadAll(jsonFile)
-	json.Unmarshal([]byte(byteValue), &sarifReport)
-
-	return &sarifReport, nil
 }
 
 // findGitRepositoryPath function finds a git repository path for a given source folder
@@ -276,14 +77,9 @@ type RepositoryMetadata struct {
 	RepoRootFolder     string
 }
 
-type ToolMetadata struct {
-	Name    string
-	Version *string
-}
-
 type ReportMetadata struct {
 	RepositoryMetadata
-	ToolMetadata
+	scaniosarif.ToolMetadata
 	Title        string
 	Time         time.Time
 	SourceFolder string
@@ -357,16 +153,6 @@ func formatDateTime(t time.Time) string {
 	return fmt.Sprintf("%s %s %d %d:%02d:%02d %s", day, t.Month(), t.Year(), t.Hour()%12, t.Minute(), t.Second(), t.Format("pm"))
 }
 
-// extractToolNameAndVersion function extracts tool name and version from a sarif report
-func extractToolNameAndVersion(sarifReport *sarif.Report) (*ToolMetadata, error) {
-	toolName := sarifReport.Runs[0].Tool.Driver.Name
-	toolVersion := sarifReport.Runs[0].Tool.Driver.SemanticVersion
-	return &ToolMetadata{
-		Name:    toolName,
-		Version: toolVersion,
-	}, nil
-}
-
 // sortResultsByLevel function sorts sarif results by level
 func sortResultsByLevel(results []*sarif.Result) {
 	// sort results by level
@@ -386,86 +172,6 @@ func sortResultsByLevel(results []*sarif.Result) {
 	})
 }
 
-// function that calculates md5 hash for a given text
-func calculateMD5Hash(text string) string {
-	hash := md5.New()
-	io.WriteString(hash, text)
-	return hex.EncodeToString(hash.Sum(nil))
-}
-
-// function that calculates a fingerprint for threadflow
-func calculateThreadFlowFingerprint(threadFlow *sarif.ThreadFlow) string {
-	var fingerprint string
-	for _, location := range threadFlow.Locations {
-		fingerprint += fmt.Sprintf("|%s:%d:%d:%d:%d;",
-			location.Location.PhysicalLocation.ArtifactLocation.Properties["URI"].(string),
-			location.Location.PhysicalLocation.Region.Properties["StartLine"].(int),
-			location.Location.PhysicalLocation.Region.Properties["StartColumn"].(int),
-			location.Location.PhysicalLocation.Region.Properties["EndLine"].(int),
-			location.Location.PhysicalLocation.Region.Properties["EndColumn"].(int),
-		)
-	}
-	return calculateMD5Hash(fingerprint)
-}
-
-// remove codeflow duplicates
-// each codeflow may have multiple threatflows. These threatflows may be equal for different codeflows.
-// This function removes duplicates from codeflows
-// if the codeflow is empty, it is removed
-func removeDataflowDuplicates(results []*sarif.Result) {
-	for _, result := range results {
-		uniqueThreadFlowsFingerprints := map[string]bool{}
-		for _, codeFlow := range result.CodeFlows {
-			uniqueThreadFlows := []*sarif.ThreadFlow{}
-			for _, threadFlow := range codeFlow.ThreadFlows {
-				fingerprint := calculateThreadFlowFingerprint(threadFlow)
-				if _, ok := uniqueThreadFlowsFingerprints[fingerprint]; !ok {
-					uniqueThreadFlowsFingerprints[fingerprint] = true
-					uniqueThreadFlows = append(uniqueThreadFlows, threadFlow)
-				}
-			}
-			codeFlow.ThreadFlows = uniqueThreadFlows
-		}
-
-		// remove empty codeflows
-		nonEmptyCodeFlows := []*sarif.CodeFlow{}
-		for _, codeFlow := range result.CodeFlows {
-			if len(codeFlow.ThreadFlows) > 0 {
-				nonEmptyCodeFlows = append(nonEmptyCodeFlows, codeFlow)
-			}
-		}
-		result.CodeFlows = nonEmptyCodeFlows
-	}
-}
-
-// function that collects information about amount of low, mediumn and high severity issues
-// returns a map with this information, and a total amount of issues
-func collectSeverityInfo(sarifReport *sarif.Report) map[string]int {
-	severityInfo := map[string]int{
-		"low":    0,
-		"medium": 0,
-		"high":   0,
-		"total":  0,
-	}
-
-	for _, run := range sarifReport.Runs {
-		for _, result := range run.Results {
-			severity := result.Properties["Level"].(string)
-			switch severity {
-			case "error":
-				severityInfo["high"]++
-			case "warning":
-				severityInfo["medium"]++
-			default:
-				severityInfo["low"]++
-			}
-			severityInfo["total"]++
-		}
-	}
-
-	return severityInfo
-}
-
 var execExampleToHTML = `  # Generate html report for semgrep sarif output
   scanio to-html --input /tmp/juice-shop/semgrep.sarif --output /tmp/juice-shop/semgrep.html --source /tmp/juice-shop`
 
@@ -478,38 +184,36 @@ var toHtmlCmd = &cobra.Command{
 		logger := logger.NewLogger(AppConfig, "core")
 		logger.Info("to-html called")
 
-		sarifReport, err := readSarifReport()
+		sarifReport, err := scaniosarif.ReadReport(allToHTMLOptions.Input, logger, allToHTMLOptions.SourceFolder)
 		if err != nil {
 			return err
 		}
 
-		enrichResultsProperties(sarifReport)
+		sarifReport.EnrichResultsProperties()
 
 		repositoryMetadata, err := collectRepositoryMetadata()
 		if err != nil {
 			logger.Debug("can't collect repository metadata", "err", err)
 		}
 
-		ToolMetadata, err := extractToolNameAndVersion(sarifReport)
+		toolMetadata, err := sarifReport.ExtractToolNameAndVersion()
 		if err != nil {
 			return err
 		}
 
-		severityInfo := collectSeverityInfo(sarifReport)
+		severityInfo := sarifReport.CollectSeverityInfo()
 
 		metadata := &ReportMetadata{
 			RepositoryMetadata: *repositoryMetadata,
-			ToolMetadata:       *ToolMetadata,
+			ToolMetadata:       *toolMetadata,
 			Title:              allToHTMLOptions.Title,
 			Time:               time.Now().UTC(),
 			SourceFolder:       allToHTMLOptions.SourceFolder,
 			SeverityInfo:       severityInfo,
 		}
 
-		for _, run := range sarifReport.Runs {
-			sortResultsByLevel(run.Results)
-			removeDataflowDuplicates(run.Results)
-		}
+		sarifReport.SortResultsByLevel()
+		sarifReport.RemoveDataflowDuplicates()
 
 		tmpl, err := template.New("report.html").
 			Funcs(template.FuncMap{
@@ -527,7 +231,7 @@ var toHtmlCmd = &cobra.Command{
 			Report   *sarif.Report
 		}{
 			Metadata: metadata,
-			Report:   sarifReport,
+			Report:   sarifReport.Report,
 		}
 
 		file, err := os.Create(allToHTMLOptions.OutputFile)
