6891958718f816f23debce8fb461cce221235153
Evgenii Protsenko <procenkoegg@gmail.com>
2024-07-17 21:41:19 +0200

    migrate all vcsurl usages to the new implementation

--- cmd/list/utils.go
+++ cmd/list/utils.go
@@ -27,7 +27,16 @@ func prepareListTarget(options *vcsintegrator.RunOptionsIntegrationVCS, args []s
 	switch mode {
 	case ModeSingleURL:
 		targetURL := args[0]
-		repoInfo, err := vcsurl.ExtractRepositoryInfoFromURL(targetURL, options.VCSPluginName)
+		vcsType := vcsurl.StringToVCSType(options.VCSPluginName)
+		url, err := vcsurl.ParseForVCSType(targetURL, vcsType)
+		repoInfo := shared.RepositoryParams{
+			Domain:        url.ParsedURL.Hostname(),
+			Namespace:     url.Namespace,
+			Repository:    url.Repository,
+			PullRequestID: url.PullRequestId,
+			HTTPLink:      url.HTTPRepoLink,
+			SSHLink:       url.SSHRepoLink,
+		}
 		if err != nil {
 			return repoInfo, fmt.Errorf("failed to extract data from provided URL '%s': %w", targetURL, err)
 		}
